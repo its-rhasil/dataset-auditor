4e78b767f16930ac9d559f007e525c1f0a08032e
Muhammed Rhasil <muhammed.rhasil7@gmail.com>
2026-07-09 23:16:24 +0530

    refactor: integrate is_categorical utility into cardinality.py

--- analyzer/detectors/cardinality.py
+++ analyzer/detectors/cardinality.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from analyzer.utils import is_categorical
 
 def cardinality(df):
     results = {}
@@ -13,14 +14,15 @@ def cardinality(df):
             top_value = df[col].value_counts().index[0]
             results[col]["top_value"] = top_value
         elif nunique == 0:
-            top_value_freq = 0
-            results[col]["nunique"] = None
-            results[col]["unique_ratio"] = None
-            results[col]["is_constant"] = None
-            results[col]["dtype_category"] = "NaN"
-            results[col]["top_value_freq"] = None
-            results[col]["cv"] = None
-            results[col]["top_value"] = None
+            results[col] = {
+                "nunique": None,
+                "unique_ratio": None,
+                "is_constant": False,
+                "dtype_category": "NaN",
+                "top_value_freq": None,
+                "cv": None,
+                "top_value": None,
+            }
             continue
         else:
             vc = df[col].value_counts(normalize = True)
@@ -29,21 +31,16 @@ def cardinality(df):
         results[col]["nunique"] = nunique
         results[col]["unique_ratio"] = unique_ratio
         results[col]["is_constant"] = is_constant
-        if pd.api.types.is_numeric_dtype(df[col]):
-            if nunique <= 10:
-                results[col]["dtype_category"] = "Categorical"
-                results[col]["top_value_freq"] = top_value_freq
-                results[col]["cv"] = None
-                results[col]["top_value"] = top_value
-            else:
-                results[col]["dtype_category"] = "Numerical"
-                results[col]["top_value_freq"] = None
-                cv = (df[col].std() / abs(df[col].mean())) if abs(df[col].mean()) > 1e-10 else np.nan
-                results[col]["cv"] = cv
-                results[col]["top_value"] = None
-        else:
+        if is_categorical(df[col]):
             results[col]["dtype_category"] = "Categorical"
             results[col]["top_value_freq"] = top_value_freq
-            results[col]["top_value"] = top_value
             results[col]["cv"] = None
+            results[col]["top_value"] = top_value
+        else:
+            results[col]["dtype_category"] = "Numerical"
+            results[col]["top_value_freq"] = None
+            cv = (df[col].std() / abs(df[col].mean())) if abs(df[col].mean()) > 1e-10 else np.nan
+            results[col]["cv"] = cv
+            results[col]["top_value"] = None
+
     return results
